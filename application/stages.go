a2b0cb96ec8fe83029ca8aac7ff1c78fd415d3c7
Sorokin Oleg <sorokin.oleg.edu@gmail.com>
2020-04-06 18:55:43 +0300

    (#41) Implement correct/incorrect unit answers

--- application/stages.go
+++ application/stages.go
@@ -1,5 +1,5 @@
 package main
 
-func getStages() []int {
-	return []int{1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377}
+func getStages() []int64 {
+	return []int64{1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377}
 }
